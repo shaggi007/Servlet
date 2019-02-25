25acd199581b04b13e8c9b3fe09c287584a30f66
Syed Ajaz Haider Zaidi <syedajaz.zaidi@gmail.com>
2019-02-25 17:38:46 +0530

    Add files via upload

---
+++ Math/src/com/math/Arithmetic_Operations.java
@@ -0,0 +1,70 @@
+package com.math;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Arithmetic_Operations
+ */
+@WebServlet("/Arithmetic_Operations")
+public class Arithmetic_Operations extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Arithmetic_Operations() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+         PrintWriter out = response.getWriter();
+		
+		String str=request.getParameter("num1");
+		String str1=request.getParameter("num2");
+		String str2=request.getParameter("operation");
+		
+		int a=Integer.parseInt(str);
+		int b=Integer.parseInt(str1);
+		
+		if(str2.equals("add")) {
+			int c=a+b;
+			out.println("Sum is:"+c);
+		}
+		if(str2.equals("sub")) {
+			int c=a-b;
+			out.println("Subtraction is:"+c);
+		}
+		if(str2.equals("mul")) {
+			int c=a*b;
+			out.println("Multiplication is:"+c);
+		}
+		if(str2.equals("div")) {
+			int c=a/b;
+			out.println("Division is:"+c);
+		}
+
+	}
+	
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
